c1adf0e960d82c73d677dba1b9772f2883be8136
evam1dshk <eva.mazdrashki@gmail.com>
2024-05-15 09:00:08 +0300

    Initial commit

---
+++ BeerShop/BeerShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BeerShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
